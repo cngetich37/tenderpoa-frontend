52fd6876884f6bbb03e85df06d7699530bfa9c83
cngetich37 <cngetich37@gmail.com>
2023-11-19 18:39:28 +0300

    updated the padding for the stepper

--- src/components/MultiStepForm.tsx
+++ src/components/MultiStepForm.tsx
@@ -78,7 +78,7 @@ export default function MultiStepForm() {
                 >
                   Add a Tender
                 </Typography>
-                <Stepper className="sm:w-1/2" activeStep={activeStep} sx={{ pt: 3, pb: 5}}>
+                <Stepper className="sm:w-1/2" activeStep={activeStep} sx={{ pt: 3, pb: 5, px:6}}>
                   {steps.map((label) => (
                     <Step key={label}>
                       <StepLabel>{label}</StepLabel>
