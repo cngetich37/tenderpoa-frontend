8b74909c72aa16655bc900b1c1055457dc182edf
cngetich37 <cngetich37@gmail.com>
2023-11-20 09:00:08 +0300

    updated steps to bid

--- src/components/StepsToBid.tsx
+++ src/components/StepsToBid.tsx
@@ -22,22 +22,68 @@ const theme = createTheme({
 
 const steps = [
   {
-    label: "Visit PPIP portal",
-    description: `For each ad campaign that you create, you can control how much
-              you're willing to spend on clicks and conversions, which networks
-              and geographical locations you want your ads to show on, and more.`,
+    label: "Identify Opportunities:",
+    description: `Monitor tender portals, government websites, or private sector platforms to identify relevant opportunities.
+    Subscribe to notifications or newsletters that provide updates on upcoming tenders in your industry.`,
   },
   {
-    label: "Create an ad group",
-    description:
-      "An ad group contains one or more ads which target a shared set of keywords.",
+    label: "Pre-Qualification",
+    description: `Ensure that your business meets the pre-qualification criteria specified in the tender documents.
+      Obtain any necessary certifications, licenses, or documentation required for eligibility`,
   },
   {
-    label: "Create an ad",
-    description: `Try out different ad text to see what brings in the most customers,
-              and learn how to enhance your ads using features like ad extensions.
-              If you run into any problems with your ads, find out how to tell if
-              they're running and how to resolve approval issues.`,
+    label: "Understand the Requirements",
+    description: `Thoroughly review the tender documents to understand the requirements, specifications, and evaluation criteria.
+    Take note of submission deadlines, formats, and any specific instructions provided.`,
+  },
+  {
+    label: "Assess Your Capabilities:",
+    description: `Evaluate your organization's capabilities to ensure that you can meet the technical, financial, and other requirements specified in the tender.`,
+  },
+  {
+    label: "Develop a Bid Team:",
+    description: `Form a bid team with members from relevant departments, such as technical, financial, legal, and management.
+    Assign specific roles and responsibilities to team members.`,
+  },
+  {
+    label: "Risk Assessment:",
+    description: `Identify potential risks associated with the tender, such as technical challenges, financial constraints, or legal issues.
+    Develop strategies to mitigate these risks and demonstrate your ability to manage them.`,
+  },
+  {
+    label: "Cost Estimation:",
+    description: `Prepare a detailed and accurate cost estimation for the project, including all relevant expenses.
+    Ensure that your pricing is competitive while covering all costs and allowing for a reasonable profit margin.`,
+  },
+  {
+    label: "Technical Proposal:",
+    description: `Develop a comprehensive technical proposal that addresses all the requirements outlined in the tender documents.
+    Clearly articulate how your solution meets or exceeds the specified criteria.`,
+  },
+  {
+    label: "Financial Proposal:",
+    description: `Prepare a detailed financial proposal that includes a breakdown of costs, pricing structure, and payment terms.
+    Ensure transparency in your financial calculations.`,
+  },
+  {
+    label: "Legal and Compliance:",
+    description: `Ensure that your bid complies with all legal requirements and regulations.
+    Review and address any legal or compliance issues that may arise.`,
+  },
+  {
+    label: "Submission:",
+    description: `Prepare the bid documents according to the specified format and submit them before the deadline.
+    Double-check that you have included all required documents and information.`,
+  },
+  {
+    label: "Follow-Up:",
+    description: `Monitor the tender process and be prepared to respond to any clarification requests from the tendering authority.
+    Attend any pre-bid meetings or site visits if required.`,
+  },
+  {
+    label: "Post-Submission Activities:",
+    description: `Prepare for negotiations if your bid is shortlisted.
+    Be responsive to any additional information or documentation requested by the tendering authority.`,
   },
 ];
 
@@ -59,14 +105,14 @@ export default function StepsToBid() {
   return (
     <div className="flex bg-white">
       <ThemeProvider theme={theme}>
-        <Box className="w-1/2 h-screen">
+        <Box className="w-1/2 h-auto">
           <Stepper activeStep={activeStep} orientation="vertical">
             {steps.map((step, index) => (
               <Step key={step.label}>
                 <StepLabel
                   optional={
                     index === 2 ? (
-                      <Typography variant="caption">Last step</Typography>
+                      <Typography variant="caption"></Typography>
                     ) : null
                   }
                 >
