6833fd5c9021fdfe40b2464957d49906e80f7b97
cngetich37 <cngetich37@gmail.com>
2023-11-23 12:15:53 +0300

    removed the checkbox selection for the already bidded component

--- src/components/AlreadyBidded.tsx
+++ src/components/AlreadyBidded.tsx
@@ -164,7 +164,7 @@ export default function AllBiddedTenders() {
                 },
               }}
               pageSizeOptions={[10, 20]}
-              checkboxSelection
+              // checkboxSelection
               slots={{ toolbar: GridToolbar }}
             />
           </Box>
