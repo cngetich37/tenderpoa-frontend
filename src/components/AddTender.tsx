d15147e869d265cd48bcf1a93ca6d99263cca8e7
cngetich37 <cngetich37@gmail.com>
2023-11-23 16:22:37 +0300

    updated the add tender component

--- src/components/AddTender.tsx
+++ src/components/AddTender.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import Grid from "@mui/material/Grid";
-// import Typography from '@mui/material/Typography';
 import Container from "@mui/material/Container";
 import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
@@ -296,9 +295,12 @@ export default function AddTender() {
                           </Grid>
                           <Grid item xs={12} md={6}>
                             <div className="mr-4">
-                            <InputLabel htmlFor="tenderFile" className="text-sm">
-                              Upload a Tender Document
-                            </InputLabel>
+                              <InputLabel
+                                htmlFor="tenderFile"
+                                className="text-sm"
+                              >
+                                Upload a Tender Document
+                              </InputLabel>
                               <TextField
                                 color="primary"
                                 required
@@ -306,8 +308,8 @@ export default function AddTender() {
                                 name="tenderFile"
                                 type="file"
                                 onChange={formik.handleChange}
-                              >
-                              </TextField>
+                                value={formik.values.tenderFile}
+                              ></TextField>
                             </div>
                           </Grid>
 
