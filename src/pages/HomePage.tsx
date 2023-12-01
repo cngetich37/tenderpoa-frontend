8e612634c3abd30d5bcf87e909d5d21d90e35644
cngetich37 <cngetich37@gmail.com>
2023-12-01 09:08:38 +0300

    updated the home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,26 +1,43 @@
-import {Link} from "react-router-dom";
-import homepic from "../assets/homepic.png"
+import { Link } from "react-router-dom";
+import homepic from "../assets/homepic.png";
 const Home = () => {
+  const styles = {
+    container: {
+      backgroundImage: `url(${homepic})`,
+      backgroundSize: 'contain', // You can use 'contain' or a specific size as well
+      height: '400px', // Adjust the height as needed
+      // You can add more styling properties as per your requirements
+    },
+  };
   return (
     <>
-      <main className="hero min-h-screen object-scale-down bg-white" style={{backgroundImage:`url(${homepic})`}}>
+      <main
+        className="hero min-h-screen object-scale-down bg-white"
+        style={styles.container}
+      >
         <div className="hero-overlay bg-opacity-60">
           <div className="hero-content text-center">
             <div className="max-w-md font-serif">
-              <h1 className="text-5xl font-bold text-white font-serif">
+              <h1 className="text-5xl font-bold text-white font-serif mt-8">
                 Tender Poa
               </h1>
               <p className="py-8 text-white">
                 Imagine a streamlined oasis for your bidding needs, where
-                efficiency meets elegance. Tender Poa is your ticket to a world of
-                effortless vendor communication and proposal evaluation. Picture a
-                digital command center, where bids flow like a symphony, and you
-                hold the conductor's baton. Say goodbye to the chaos of
-                spreadsheets and emails, and say hello to a future where your
-                tendering process is a work of art, all within a few clicks. This
-                is the epitome of tendering sophistication, made simple for you.
+                efficiency meets elegance. Tender Poa is your ticket to a world
+                of effortless vendor communication and proposal evaluation.
+                Picture a digital command center, where bids flow like a
+                symphony, and you hold the conductor's baton. Say goodbye to the
+                chaos of spreadsheets and emails, and say hello to a future
+                where your tendering process is a work of art, all within a few
+                clicks. This is the epitome of tendering sophistication, made
+                simple for you.
               </p>
-              <Link to="/addtender" className="btn bg-[#800000] text-white hover:text-white hover:bg-purple-400">Make a Bid</Link>
+              <Link
+                to="/addtender"
+                className="btn bg-[#800000] text-white hover:text-white hover:bg-purple-400"
+              >
+                Getting Started
+              </Link>
             </div>
           </div>
         </div>
