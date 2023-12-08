6de12dd996b3c623866af2a27614e8f6c8782c09
cngetich37 <cngetich37@gmail.com>
2023-12-08 09:39:19 +0300

    updated the missing page component

--- src/components/MissingPage.tsx
+++ src/components/MissingPage.tsx
@@ -2,7 +2,7 @@ const MissingPage = () => {
   return (
     <>
       <div className="hero h-screen bg-white flex items-start justify-center">
-        <div className="hero-content mt-12">
+        <div className="hero-content mt-20">
           <div className="text-[#800000] text-4xl font-bold">
             Something went wrong!
           </div>
